8ac41a55d1d64daf5c84ef6598fad78c7497208e
tisco <tisco.sl@gmail.com>
2018-10-11 20:04:58 -0300

    Curso em Video Python -  Mundo 2

---
+++ Desafio 41 -  Classificando Atletas.py
@@ -0,0 +1,17 @@
+import datetime
+nascimento = 1
+while True:
+    nascimento = int(input('ano do Nascimento [0 para sair]: '))
+
+    if datetime.date.today().year - nascimento <= 9:
+        print(f'{datetime.date.today().year - nascimento} anos, categoria MIRIM')
+    elif 9 < datetime.date.today().year - nascimento <= 14:
+        print(f'{datetime.date.today().year - nascimento} anos, categoria INFANTIL')
+    elif 14 < datetime.date.today().year - nascimento <= 19:
+        print(f'{datetime.date.today().year - nascimento} anos, categoria JUNIOR')
+    elif 19 < datetime.date.today().year - nascimento <= 25:
+        print(f'{datetime.date.today().year - nascimento} anos, categoria SÊNIOR')
+    elif nascimento == 0:
+        break
+    else:
+        print(f'{datetime.date.today().year - nascimento} anos, categoria MASTER')
