0d347619d6eca3daa5ddc1da2e3570632b415e8c
tisco <tisco.sl@gmail.com>
2018-10-10 23:15:23 -0300

    Desafio 38

---
+++ Desafio 38 -  Comparando numeros.py
@@ -0,0 +1,9 @@
+n1 = int(input('Valor 1: '))
+n2 = int(input('Valor 2: '))
+
+if n1 > n2:
+    print(f'O valor {n1} é maior que {n2}')
+elif n2 > n1:
+    print(f'O valor {n2} é maior que {n1}')
+else:
+    print(f'os valores 1 e 2 são iguais e valem {n1}')
