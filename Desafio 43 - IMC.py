1536427ced4c86f64194fef149d17171aad4288d
tisco <tisco.sl@gmail.com>
2018-10-13 13:15:51 -0300

    Curso em Vídeo Python 3 - Mundo 2

--- Desafio 43 - IMC.py
+++ Desafio 43 - IMC.py
@@ -4,13 +4,13 @@ altura = float(input('ALtura (m): '))
 print(f"""\n{'Dados pessoais':*^60}\nPeso: {peso} kg
 Altura: {altura} m
 IMC: {(peso/(altura**2)):.2f}\n{'*'*60}\n""")
-if (peso/(altura**2)) <=18.5:
+if (peso / (altura ** 2)) <= 18.5:
     print('Você se encontra ABAIXO DO PESO IDEAL')
-elif 18.5 <(peso/(altura**2)) <=25:
+elif 18.5 < (peso / (altura ** 2)) <= 25:
     print('Você se encontra NO PESO IDEAL')
-elif 25 <(peso/(altura**2)) <=30:
+elif 25 < (peso / (altura ** 2)) <= 30:
     print('Você se encontra NA FAIXA DE SOBREPESO')
-elif 30 <(peso/(altura**2)) <=40:
+elif 30 < (peso / (altura ** 2)) <= 40:
     print('Você se encontra NA FAIXA DE OBESIDADE')
 else:
     print('Você se encontra NA FAIXA DE OBESIDADE MÓRBIDA')
