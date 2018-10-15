f6c8216fb3484b11b3c53fd728dbaf71d1c2f5a8
tisco <tisco.sl@gmail.com>
2018-10-15 19:14:44 -0300

    Curso em Video Python 3 - Mundo 2

---
+++ Desafio 59 - Menu de opções.py
@@ -0,0 +1,26 @@
+op = 0
+n1 = int(input('Valor 1: '))
+n2 = int(input('Valor 2: '))
+
+while op!= 5:
+    op = int(input(""""O Que deseja fazer?
+    [1] Somar
+    [2] Multipicar
+    [3] Maior
+    [4] Novos Numeros
+    [5] sair
+    QUAL SUA OPÇÃO: """))
+    if op == 1:
+        print(f'{n1} + {n2} = {n1+n2}')
+    elif op == 2:
+        print(f'{n1} X {n2} = {n1*n2}')
+    elif op == 3:
+        if n1 > n2:
+            print(f'{n1} é maior que {n2}')
+        else:
+            print(f'{n2} é maior que {n1}')
+    elif op == 4:
+        n1 = int(input('Novo Valor 1: '))
+        n2 = int(input('Novo valor 2: '))
+    else:
+        print('Opção Inválida! Tente novamente.')
