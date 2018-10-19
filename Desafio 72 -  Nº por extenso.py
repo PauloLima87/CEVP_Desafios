be903754014327b4005ef868ec2e8da94cd191be
tisco <tisco.sl@gmail.com>
2018-10-19 17:58:27 -0300

    Curso em Video Python 3 - Mundo 3

--- Desafio 72 -  Nº por extenso.py
+++ Desafio 72 -  Nº por extenso.py
@@ -2,10 +2,10 @@ num = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito',
        'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
 
 while True:
-    op = int(input('Digite um numero de 0 a 20: '))
-    if op <= 20:
-        print(num[op])
-    elif op >20:
-        print('NUmero inválido!')
+    op = int(input('Digite um numero de 0 a 20 [999 para finalizar]: '))
+    if 0 <= op <= 20:
+        print(f'Você digitou o numero {num[op]}')
     elif op == 999:
         break
+    elif op > 20 or op < 0:
+        print('Número inválido!')
