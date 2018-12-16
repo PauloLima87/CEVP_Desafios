23fecc6a12ec3f833bdc306dd19298bf4163f3e3
tisco <tisco.sl@gmail.com>
2018-12-16 11:39:10 -0200

    Curso em Video Python 3 - Mundo 3

---
+++ Desafio 78 - Maior e Menor valor.py
@@ -0,0 +1,21 @@
+valor = list()
+menor = maior = 0
+for i in range(0, 5):
+    valor.append(int(input(f'Valor {i + 1}: ')))
+    if i == 1:
+        maior = menor = valor[i]
+    else:
+        if valor[i] > maior:
+            maior = valor[i]
+        if valor[i] < menor:
+            menor = valor[i]
+
+print(f"O Menor valor é {menor} nas posições ", end='')
+for a, b in enumerate(valor):
+    if b == menor:
+        print(f'{a}.. ', end='')
+print()
+print(f"O Maior valor é {maior} nas posições ", end='')
+for a,b in enumerate(valor):
+    if b == maior:
+        print(f'{a}.. ', end='')
